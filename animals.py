1c41596c640108d63f95872a0c4a700b26b60f91
Stacy Griffy <sgriffy2@gmail.com>
2024-02-21 16:42:42 -0600

    refactored critter classes and condensed code

--- animals.py
+++ animals.py
@@ -2,209 +2,157 @@
 from datetime import date
 
 
-# petting area
 class Donkey:
-
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
 
 class Llama:
-
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
 
 class Goat:
-
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
 
 class Pony:
-
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
 
 class Pig:
-
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
 
-# glass tank
+# Glass Tank
 class Copperhead:
-
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = True
 
 
 class RatSnake:
-
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = True
 
 
 class BoaConstrictor:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = True
 
 
 class GarterSnake:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = True
 
 
 class Anaconda:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = True
 
 
-# pond
+# Pond
 class Mallard:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
 
 class Goldfish:
-
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
 
 class Frog:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
 
 class Turtle:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
 
 class Swan:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
 
-eeyore = Donkey()
-miss_fuzz = Llama()
-billy = Goat()
-sir_gallop_a_lot = Pony()
-hamlet = Pig()
-slytherin = Copperhead()
-sneaky_pete = RatSnake()
-mr_squeeze = BoaConstrictor()
-slinky_joe = GarterSnake()
-monty = Anaconda()
-quacky = Mallard()
-goldie = Goldfish()
-croakster = Frog()
-shelly = Turtle()
-gracie = Swan()
+eeyore = Donkey("Eeyore", "Sad Donkey")
+miss_fuzz = Llama("Miss Fuzz", "Domestic Llama")
+billy = Goat("Billy", "Playful Goat")
+sir_gallop_a_lot = Pony("Sir Gallop-a-Lot", "Miniature Pony")
+hamlet = Pig("Hamlet", "Adorable Pig")
+slytherin = Copperhead("Slytherin", "Venomous Snake")
+sneaky_pete = RatSnake("Sneaky Pete", "Sly Snake")
+mr_squeeze = BoaConstrictor("Mr. Squeeze", "Gentle Giant")
+slinky_joe = GarterSnake("Slinky Joe", "Smooth Operator")
+monty = Anaconda("Monty", "Big Squeeze")
+quacky = Mallard("Quacky", "Cheerful Mallard")
+goldie = Goldfish("Goldie", "Shiny Goldfish")
+croakster = Frog("Croakster", "Jumping Frog")
+shelly = Turtle("Shelly", "Slow Turtle")
+gracie = Swan("Gracie", "Elegant Swan")
 
-eeyore.name = "Eeyore"
-eeyore.species = "donkey"
-print(eeyore.name)
 
-miss_fuzz.name = "Miss Fuzz"
-miss_fuzz.species = "domestic llama"
-print(miss_fuzz.name)
-
-billy.name = "Billy"
-billy.species = "goat"
-print(billy.name)
-
-sir_gallop_a_lot.name = "Sir Gallop-A-Lot"
-sir_gallop_a_lot.species = "pony"
-print(sir_gallop_a_lot.name)
-
-hamlet.name = "Hamlet"
-hamlet.species = "pig"
-print(hamlet.name)
-
-slytherin.name = "Slytherin"
-slytherin.species = "copperhead"
-print(slytherin.name)
-
-sneaky_pete.name = "Sneaky Pete"
-sneaky_pete.species = "rat snake"
-print(sneaky_pete.name)
-
-mr_squeeze.name = "Mr. Squeeze"
-mr_squeeze.species = "boa constrictor"
-print(mr_squeeze.name)
-
-slinky_joe.name = "Slinky Joe"
-slinky_joe.species = "garter snake"
-print(slinky_joe.name)
-
-monty.name = "Monty"
-monty.species = "anaconda"
-print(monty.name)
-
-quacky.name = "Quacky"
-quacky.species = "mallard"
-print(quacky.name)
-
-goldie.name = "Goldie"
-goldie.species = "goldfish"
-print(goldie.name)
-
-croakster.name = "Croakster"
-croakster.species = "frog"
-print(croakster.name)
-
-shelly.name = "Shelly"
-shelly.species = "turtle"
-print(shelly.name)
-
-gracie.name = "Gracie"
-gracie.species = "swan"
-print(gracie.name)
+print(f"{eeyore.name} the {eeyore.species}")
+print(f"{miss_fuzz.name} the {miss_fuzz.species}")
+print(f"{billy.name} the {billy.species}")
+print(f"{sir_gallop_a_lot.name} the {sir_gallop_a_lot.species}")
+print(f"{hamlet.name} the {hamlet.species}")
+print(f"{slytherin.name} the {slytherin.species}")
+print(f"{sneaky_pete.name} the {sneaky_pete.species}")
+print(f"{mr_squeeze.name} the {mr_squeeze.species}")
+print(f"{slinky_joe.name} the {slinky_joe.species}")
+print(f"{monty.name} the {monty.species}")
+print(f"{quacky.name} the {quacky.species}")
+print(f"{goldie.name} the {goldie.species}")
+print(f"{croakster.name} the {croakster.species}")
+print(f"{shelly.name} the {shelly.species}")
+print(f"{gracie.name} the {gracie.species}")
