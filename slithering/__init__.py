09ab928e52e67a0a0eb4a5bbac0959537b779adc
Stacy Griffy <sgriffy2@gmail.com>
2024-02-21 18:06:56 -0600

    Separated classes into individual modules

---
+++ slithering/__init__.py
@@ -0,0 +1,5 @@
+from .anaconda import Anaconda
+from .boa_constrictor import BoaConstrictor
+from .copperhead import Copperhead
+from .garter_snake import GarterSnake
+from .rat_snake import RatSnake
---
+++ swimming/__init__.py
@@ -0,0 +1,5 @@
+from .frog import Frog
+from .goldfish import Goldfish
+from .mallard import Mallard
+from .swan import Swan
+from .turtle import Turtle
